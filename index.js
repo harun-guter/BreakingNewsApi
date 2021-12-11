29bf6c221c3f7f1555bc552c6fd6adb9cda5a2be
Harun Guter <harunguter@gmail.com>
2021-12-11 12:56:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const getNews = require('./news');
 const app = express();
 
 app.get('/api/news', (request, response) => {
+    response.contentType('application/json');
     getNews.then((data) => response.send({
         totalNews: data.length,
         news: data,
     }))
-    console.log(getNews);
+});
+
+app.get('/api/news/*', (request, response) => {
+    response.contentType("application/json");
+    getNews.then((data) => {
+        data.find(x => x.id === 1 * request.url.split('/')[3]) !== undefined ?
+            response.send(data.find(x => x.id === (1 * request.url.split('/')[3]))) :
+            response.redirect('/api/news');
+    })
 });
 
 app.listen(3000);
